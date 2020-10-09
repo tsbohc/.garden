6d746249b5ee5759c6c282aa69d4bb43c43c05cd
seancallous <justawinter@gmail.com>
2020-10-09 04:05:50 +0300

    ytparse

--- src/fytui/yt.py
+++ src/fytui/yt.py
@@ -12,25 +12,37 @@ youtube_channel_ids = [
     ''
 ]
 
+subreddits = [
+    'unixporn'
+]
+
 def pretty_print(j):
     print(json.dumps(j, indent=4))
 
 def exception_handler(request, exception):
     print("Request failed", exception)
 
-for i in youtube_channel_ids:
-    urls.append('http://www.youtube.com/feeds/videos.xml?channel_id=' + i)
+#for i in youtube_channel_ids:
+    #urls.append('http://www.youtube.com/feeds/videos.xml?channel_id=' + i)
 
-requests = (grequests.get(u, stream=True) for u in urls)
+for i in subreddits:
+    urls.append('https://old.reddit.com/r/' + i + '.rss')
+
+headers = {
+    'User-Agent': 'aikrtionrstdirpdrrstd'
+}
+
+requests = (grequests.get(u, headers=headers, stream=True) for u in urls)
 responces = (grequests.map(requests, exception_handler=exception_handler))
 
 for r in responces:
+    # can check url to determine what to extract
+    print(r.url)
     if r.status_code != 200:
         continue
     f = feedparser.parse(r.text)
 
-    channel = f['feed']['title']
-
+    # youtube parse
     for entry in f['entries']:
 
         published = entry['published']
@@ -38,12 +50,13 @@ for r in responces:
         published = str(datetime.strptime(published, "%Y-%m-%dT%H:%M:%S").timestamp())
 
         rss.append({
-            'channel': channel,
+            'type': 'youtube',
+            'author': f['feed']['title'],
             'title': entry['title'],
             'summary': entry['summary'],
-            'url': entry['link'],
-            'image': entry['media_thumbnail'][0]['url'],
             'published': published
+            'link': entry['link'],
+            'thumbnail': entry['media_thumbnail'][0]['url'],
         })
 
-pretty_print(rss)
+#pretty_print(rss)
