27520fba9c9d9dbd814576f1a2d5e21d15f37ca2
seancallous <justawinter@gmail.com>
2020-10-14 16:31:57 +0300

    fytui reddit

--- src/fytui/yt.py
+++ src/fytui/yt.py
@@ -17,7 +17,10 @@ youtube_channel_ids = [
 ]
 
 subreddits = [
-    'unixporn'
+    #'unixporn',
+    #'commandline',
+    #'vim'
+    'all'
 ]
 
 def pretty_print(j):
@@ -26,11 +29,11 @@ def pretty_print(j):
 def exception_handler(request, exception):
     print("Request failed", exception)
 
-for i in youtube_channel_ids:
-    urls.append('http://www.youtube.com/feeds/videos.xml?channel_id=' + i)
+#for i in youtube_channel_ids:
+#    urls.append('http://www.youtube.com/feeds/videos.xml?channel_id=' + i)
 
-#for i in subreddits:
-    #urls.append('https://old.reddit.com/r/' + i + '.rss')
+for i in subreddits:
+    urls.append('https://reddit.com/r/' + i + '.json')
 
 headers = {
     'User-Agent': 'aikrtionrstdirpdrrstd'
@@ -40,28 +43,51 @@ requests = (grequests.get(u, headers=headers, stream=True) for u in urls)
 responces = (grequests.map(requests, exception_handler=exception_handler))
 
 for r in responces:
-    # can check url to determine what to extract
-    #print(r.url)
     if r.status_code != 200:
         continue
-    f = feedparser.parse(r.text)
 
-    # youtube parse
-    for entry in f['entries']:
+    if "youtube.com" in r.url:
+        f = feedparser.parse(r.text)
+        for entry in f['entries']:
 
-        published = entry['published']
-        published = published.split('+', 1)[0]
-        published = datetime.strptime(published, "%Y-%m-%dT%H:%M:%S").timestamp()
+            published = entry['published']
+            published = published.split('+', 1)[0]
+            published = datetime.strptime(published, "%Y-%m-%dT%H:%M:%S").timestamp()
 
-        rss.append({
-            'type': 'youtube',
-            'author': f['feed']['title'],
-            'title': entry['title'].replace('/', '|').replace("\'", "").replace("\"", ""),
-            'summary': entry['summary'],
-            'published': published,
-            'link': entry['link'],
-            'thumbnail': entry['media_thumbnail'][0]['url']
-        })
+            rss.append({
+                'type': 'youtube',
+                'author': f['feed']['title'],
+                'title': entry['title'].replace('/', '|').replace("\'", "").replace("\"", "").replace("|", "::"),
+                'summary': entry['summary'],
+                'published': published,
+                'link': entry['link'],
+                'thumbnail': entry['media_thumbnail'][0]['url']
+            })
+
+    elif "reddit.com" in r.url:
+        f = json.loads(r.text)
+        for entry in f['data']['children']:
+            entry = entry['data']
+            #print(entry["title"])
+
+            if "url_overridden_by_dest" in entry:
+                target = entry["url_overridden_by_dest"]
+            else:
+                target = ""
+
+            rss.append({
+                'type': 'reddit',
+                'author': entry['subreddit'],
+                'title': entry['title'].replace('/', '|').replace("\'", "").replace("\"", "").replace("|", "::"),
+                'score': entry['ups'] - entry['downs'],
+                'summary': entry['selftext'],
+                'published': entry['created'],
+                'perma': 'https://reddit.com' + entry['permalink'],
+                'thumbnail': entry['thumbnail'],
+                'link': target
+                #entry["preview"] #["images"][0]["source"]["url"]
+            })
+
 
 #pretty_print(rss)
 
@@ -74,6 +100,8 @@ urls={}
 for i in rss:
     time.sleep(0.0001) # enough to allow sorting by date
     with open("out/" + i["author"] + " - " + i["title"], 'w') as o:
+        if "score" in i:
+            o.write("[" + str(i["score"]) + "] ")
         o.write(i["title"])
         o.write("\n")
         o.write(i["author"])
@@ -85,13 +113,14 @@ for i in rss:
         o.write(i["summary"])
         o.write("\n")
         o.write(i["link"])
-        urls[i["thumbnail"]] = i["author"] + " - " + i["title"]
 
-requests = (grequests.get(u, headers=headers, stream=True) for u in urls.keys())
-responces = (grequests.map(requests, exception_handler=exception_handler))
+        #urls[i["thumbnail"]] = i["author"] + " - " + i["title"]
 
-for r in responces:
-    if r.status_code != 200:
-        continue
-    with open("yttmb/" + urls[r.url] + ".jpg", 'wb') as f:
-        f.write(r.content)
+#requests = (grequests.get(u, headers=headers, stream=True) for u in urls.keys())
+#responces = (grequests.map(requests, exception_handler=exception_handler))
+#
+#for r in responces:
+#    if r.status_code != 200:
+#        continue
+#    with open("yttmb/" + urls[r.url] + ".jpg", 'wb') as f:
+#        f.write(r.content)
