6258f04f36029fbcdfdc78b8e1b3f8ec1072b9e3
sean <justawinter@gmail.com>
2021-07-31 05:47:54 +0300

    rake rewrite in pure bash

--- etc/test/leaf.sh
+++ etc/test/leaf.sh
@@ -1,4 +1,12 @@
 rake() {
+  echo "running rake in test!"
   link $(render template) "~/.config/template"
   link link "~/.config/link"
+}
+
+unrake() {
+  echo "unrake in test!"
+  # can use this to remove dirs, like .config/polybar
+  unlink "~/.config/link"
+  unlink "~/.config/template"
 }
